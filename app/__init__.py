9627ba71d59c900b04716dd75149caf8b631de0e
Zixi Qiao <qiaozixi@gmail.com>
2026-03-31 02:06:31 +0000

    minimal flask app in place

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+# Zixi Qiao, Robert Chen, Kalimul Kaif, Cody Wong
+# HalfGone
+# SoftDev
+# P04 - Makers Makin' It, Act II -- THe Seequel
+# 2026-04-20
+# time spent: 1
+
+from flask import Flask, render_template, redirect, url_for
+app = Flask(__name__)           #create instance of class Flask
+app.secret_key = 'half_gone'
+
+@app.route("/", methods = ['GET', 'POST'])
+def home():
+    return "No hablo queso!"
+
+if __name__ == "__main__":      # true if this file NOT imported
+    app.debug = True            # enable auto-reload upon code change
+    app.run(host = '0.0.0.0', port = 5000)
