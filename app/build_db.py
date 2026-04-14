96e92a3d37c77d928704c270d26b92a6bc8c8905
Zixi Qiao <qiaozixi@gmail.com>
2026-04-13 21:10:32 -0400

    update db to drop tables + misc

--- app/build_db.py
+++ app/build_db.py
@@ -6,36 +6,37 @@
 # time spent: 1
 
 import sqlite3
-
 DB_FILE = "data.db"
 
 SQL_SCHEMA = """
-PRAGMA foreign_keys = ON;
-
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS stocks;
 DROP TABLE IF EXISTS stock_data;
+DROP TABLE IF EXISTS stocks;
+DROP TABLE IF EXISTS users;
+
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
-  username TEXT UNIQUE,
-  password TEXT
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE stocks (
   id INTEGER PRIMARY KEY,
-  ticker TEXT,
-  company_name TEXT
+  ticker TEXT UNIQUE NOT NULL,
+  company_name TEXT NOT NULL
 );
 
 CREATE TABLE stock_data (
-  stock_id INTEGER,
-  date TIMESTAMP,
+  stock_id INTEGER NOT NULL,
+  date TIMESTAMP NOT NULL,
   open_price FLOAT,
   close_price FLOAT,
   returns FLOAT,
   volume INTEGER,
-  FOREIGN KEY(stock_id) REFERENCES stocks(id)
+  PRIMARY KEY (stock_id, date),
+  FOREIGN KEY (stock_id) REFERENCES stocks(id)
 );
 """
 
